7debcd5d4ca847d724a2fd1ef7795b2e1eb4bbb3
Kyle McDonald <kyle@kylemcdonald.net>
2021-11-17 00:16:19 -0800

    fill out and simplify region distribution diagram

--- estimate_emissions_factors.py
+++ estimate_emissions_factors.py
@@ -10,9 +10,7 @@ from block_index import BlockIndex
 from block_classifier import BlockClassifier
 
 import datetime
-import json
-from collections import defaultdict, Counter
-from itertools import islice
+from collections import defaultdict
 
 plt.rcParams['font.size'] = 20
 
@@ -47,6 +45,18 @@ for miner in mining_regions.index:
         continue
     factors = sum([w*f for w,f in factors]) / sum([w for w,f in factors])
     lookup_emissions_factor_by_miner[miner] = factors.to_dict()
+    
+# build lookup from mining pool regions to normalized weights
+normalized_mining_pool_regions = normalize_row_to_one(mining_regions[region_names])
+miner_to_weights = {}
+for miner in mining_regions.index:
+    weights = {}
+    for region_name in region_names:
+        weight = normalized_mining_pool_regions.loc[miner, region_name]
+        if weight != weight:
+            continue
+        weights[region_name] = weight
+    miner_to_weights[miner] = weights
 
 # this is where we classify extraData or mining pool into an emissions factor
 extra_data_samples = []
@@ -54,6 +64,8 @@ miner_samples = []
 emissions_factor_samples = []
 known_by_extra_data = 0
 known_by_miner = 0
+
+region_distribution = defaultdict(lambda: defaultdict(float))
 
 total = index.latest_block() - 1
 for block in tqdm(index.list_blocks(skip_genesis=True), total=total):
@@ -66,6 +78,7 @@ for block in tqdm(index.list_blocks(skip_genesis=True), total=total):
         label = 'unknown'
     elif label in lookup_emissions_factor_by_label:
         factor = lookup_emissions_factor_by_label[label][date.year]
+        region_distribution[date][label] += 1
         sample = (date, factor)
         known_by_extra_data += 1
     extra_data_samples.append((date, label))
@@ -75,12 +88,17 @@ for block in tqdm(index.list_blocks(skip_genesis=True), total=total):
         label = 'unknown'
     elif sample is None and label in lookup_emissions_factor_by_miner:
         factor = lookup_emissions_factor_by_miner[label][date.year]
+        for region, weight in miner_to_weights[label].items():
+            region_distribution[date][region] += weight
         sample = (date, factor)
         known_by_miner += 1
     miner_samples.append((date, label))
     
-    if sample is not None:
-        emissions_factor_samples.append(sample)
+    if sample is None:
+        region_distribution[date]['unknown'] += 1
+        continue
+        
+    emissions_factor_samples.append(sample)
 
 def list_dates(start_date, end_date):
     dates = []
@@ -135,71 +153,64 @@ counts = {
 }
 for k,v in counts.items():
     results[f'{k}_count'] = v
-
-def daily_distribution(samples, normalize=True):
-    samples.sort()
     
-    begin_date = samples[0][0]
-    end_date = samples[-1][0]
-    dates = list_dates(begin_date, end_date)
-
-    unique_labels = list(sorted(set([e[1] for e in samples])))
-    label_to_index = {e:i for i,e in enumerate(unique_labels)}
-    by_label = np.zeros((len(unique_labels), len(dates)), int)
-
-    for date, label in samples:
-        index = label_to_index[label]
-        offset = (date - begin_date).days
-        by_label[index, offset] += 1
-
-    normalized = by_label.astype(float)
-    if normalize:
-        normalized /= normalized.sum(0)
-    normalized[np.isnan(normalized)] = 0
-    
-    return dates, unique_labels, normalized
-
 def draw_ratios(dates, labels, data, legend=False, legend_n=8):
     indices = np.argsort(data.sum(1))[::-1]
     fig = plt.figure(figsize=(20,5), facecolor='white')
-    plt.stackplot(dates, data[indices], lw=0, labels=[labels[i] for i in indices])
+    plt.stackplot(dates, 100*data[indices], lw=0, labels=[labels[i] for i in indices])
     plt.xlim(dates[0], dates[-1])
     
     if data.max() == 1:
-        plt.ylim(0,1)
+        plt.ylim(0,100)
     
     if legend:
         n = len(labels)
         plt.legend(ncol=min(n,legend_n),
                    loc='upper center',
                    bbox_to_anchor=(0.5, -0.1))
+        
+    plt.yticks([])
 
     return fig
-    
-def draw_samples(samples, normalize=True, legend=True, **kwargs):
-    return draw_ratios(*daily_distribution(samples, normalize=normalize), legend=legend, **kwargs)
 
-to_collapse = set([
-    'ukraine',
-    'us-west',
-    'russia',
-    'taiwan',
-    'us-east',
-    'singapore'])
-collapsed = []
-for d,e in extra_data_samples:
-    if e in to_collapse:
-        collapsed.append((d, 'other'))
-    else:
-        collapsed.append((d, e))
+collapse_map = {
+    'asia': 'Asia',
+    'china': 'China',
+    'europe': 'Europe',
+    'europe-north': 'Europe',
+    'europe-west': 'Europe',
+    'russia': 'Europe',
+    'seoul': 'Asia',
+    'singapore': 'Asia',
+    'taiwan': 'Asia',
+    'ukraine': 'Europe',
+    'us': 'USA',
+    'us-east': 'USA',
+    'us-west': 'USA',
+    'unknown': 'Unknown'
+}
 
-fig = draw_samples(collapsed, normalize=True, legend_n=5)
+region_distribution_collapsed = defaultdict(lambda: defaultdict(float))
+for date, dist in region_distribution.items():
+    for region, count in dist.items():
+        region_distribution_collapsed[date][collapse_map[region]] += count
+
+region_distribution_normalized = normalize_row_to_one(pd.DataFrame(region_distribution).T).sort_index()
+dates = region_distribution_normalized.index.values
+labels = region_distribution_normalized.columns.values
+data = region_distribution_normalized.values
+data[np.isnan(data)] = 0
+
+# save for visualization
+pd.DataFrame(region_distribution).T.round().to_csv('output/all_regions.csv', index=False, float_format='%.0f')
+region_distribution_normalized.to_csv('output/collapsed_regions.csv', index=False, float_format='%.4f')
+
+fig = draw_ratios(dates, labels, data.T, legend=True)
 ban1 = datetime.datetime(2021,5,21).date()
 ban2 = datetime.datetime(2021,9,23).date()
 plt.axvline(ban1, color='w', lw=1, linestyle='--')
 plt.axvline(ban2, color='w', lw=1, linestyle='--')
-plt.yticks([])
-plt.savefig('overleaf/images/extra-data-stackplot.png', bbox_inches='tight')
+plt.savefig('overleaf/images/region-stackplot.png', bbox_inches='tight')
 plt.close(fig)
 
 results.write()
