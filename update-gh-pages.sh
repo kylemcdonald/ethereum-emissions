78df22e4f940e1b19128f547508330f661dcbc7c
Kyle McDonald <kyle@kylemcdonald.net>
2022-07-21 18:38:48 -0700

    no-history update process

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -11,8 +11,12 @@ mkdir -p backup
 mv output/daily-gw.csv output/daily-ktco2.csv backup
 git reset --hard # need to do this otherwise we are asked to commit input/hashrate.csv
 git checkout gh-pages
+git pull origin
+git branch -D flattened
+git checkout --orphan flattened
 cp backup/* output/
-git add -u output/
+rm -rf backup
+git add .
 git commit -m "update"
-git push origin gh-pages
+git push origin +flattened:gh-pages
 git checkout main
